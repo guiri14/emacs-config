1d0a215b3b46da5a1214c32156822ce7dbde720e
Alex Fontenot <joseph.fontenot@acadian.com>
2017-06-27 15:50:00 -0500

    Fixes some helm movement keys

--- init.el
+++ init.el
@@ -1,3 +1,7 @@
+(defun open-config ()
+  (interactive)
+  (find-file user-init-file))
+
 (require 'package)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
@@ -19,11 +23,26 @@
 (require 'evil)
 (evil-mode 1)
 
+(require 'helm)
 (require 'helm-config)
-(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "M-x") #'helm-M-x)
 (global-set-key (kbd "C-x b") 'helm-mini)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
 
-(defun open-config ()
-  (interactive)
-  (find-file user-init-file))
+(define-key helm-map (kbd "C-j") 'helm-next-line)
+(define-key helm-map (kbd "C-k") 'helm-previous-line)
+
+;; Unbind the two emacs movement keys
+(define-key helm-map (kbd "C-n") nil)
+(define-key helm-map (kbd "C-p") nil)
+
+(setq x-select-enable-clipboard t)
+(setq global-linium-mode t)
+
+(defvar --backup-directory (concat user-emacs-directory "backups"))
+(if (not (file-exists-p --backup-directory))
+    (make-directory --backup-directory t))
+
+(setq backup-directory-alist `(("." . , --backup-directory)))
+
+(setq delete-old-versions t)
